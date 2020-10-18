a28b1c68b7acd3b08a152c9c1de1c347dae24006
Tanya Rezo <32035077+tanya-rezo@users.noreply.github.com>
2020-10-18 17:52:33 +0300

    Подключение form-validation.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 import jQuery from "jquery";
 import popper from "popper.js";
 import bootstrap from "bootstrap";
+import "./form-validation";
+
 
 // jQuery(function() {
 //   jQuery("body").css("color", "blue");
