78888e1e9302c991f86db3c3bb8464e431eb9255
Tanya Rezo <32035077+tanya-rezo@users.noreply.github.com>
2020-12-21 01:47:04 +0300

    Логика кнопок возраста

--- src/js/form-validation.js
+++ src/js/form-validation.js
@@ -1,7 +1,35 @@
-document.getElementById("send-btn").addEventListener("click", function() {
-     if (document.getElementById("firstName").value == "" || document.getElementById("phoneNumber").value == "") {
-        window.location.assign("error.html");
-    } else {
-        window.location.assign("success.html");
-    }
-});
+var send_btn = document.getElementById("send-btn");
+if (send_btn != null) {
+    send_btn.addEventListener("click", function() {
+        if (document.getElementById("firstName").value == "" || document.getElementById("phoneNumber").value == "") {
+            window.location.assign("error.html");
+        } else {
+            window.location.assign("success.html");
+        }
+    });
+}
+
+
+var age_btn_minus = document.getElementById("age-btn-minus");
+if (age_btn_minus != null) {
+    age_btn_minus.addEventListener("click", function() {
+        var input = document.getElementById("age-input");
+
+        var age = +input.value;
+        age = age - 1;
+        if (age >= 0) {
+            input.value = age;
+        }
+    });
+}
+
+var age_btn_plus = document.getElementById("age-btn-plus");
+if (age_btn_plus != null) {
+    age_btn_plus.addEventListener("click", function() {
+        var input = document.getElementById("age-input");
+
+        var age = +input.value;
+        age = age + 1;
+        input.value = age;
+    });
+}
