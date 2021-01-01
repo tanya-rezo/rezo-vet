5333a6cfadbeeea607654e0ba0759b4af72f7232
Tanya Rezo <32035077+tanya-rezo@users.noreply.github.com>
2021-01-02 00:00:20 +0300

    Починка логики и позиционирование текста дропдауна

--- src/js/appointment.js
+++ src/js/appointment.js
@@ -1,4 +1,4 @@
-var send_btn = document.getElementById("send-btn");
+let send_btn = document.getElementById("send-btn");
 if (send_btn != null) {
     send_btn.addEventListener("click", function() {
         if (document.getElementById("firstName").value == "" || document.getElementById("phoneNumber").value == "") {
@@ -10,12 +10,12 @@ if (send_btn != null) {
 }
 
 
-var age_btn_minus = document.getElementById("age-btn-minus");
+let age_btn_minus = document.getElementById("age-btn-minus");
 if (age_btn_minus != null) {
     age_btn_minus.addEventListener("click", function() {
-        var input = document.getElementById("age-input");
+        let input = document.getElementById("age-input");
 
-        var age = +input.value;
+        let age = +input.value;
         age = age - 1;
         if (age >= 0) {
             input.value = age;
@@ -23,22 +23,22 @@ if (age_btn_minus != null) {
     });
 }
 
-var age_btn_plus = document.getElementById("age-btn-plus");
+let age_btn_plus = document.getElementById("age-btn-plus");
 if (age_btn_plus != null) {
     age_btn_plus.addEventListener("click", function() {
-        var input = document.getElementById("age-input");
+        let input = document.getElementById("age-input");
 
-        var age = +input.value;
+        let age = +input.value;
         age = age + 1;
         input.value = age;
     });
 }
 
-var dropdowns = document.getElementsByClassName("dropdown");
-for (var dropdown of dropdowns) {
-    var button = dropdown.children[0];
-    var items = dropdown.children[1].children;
-    for (var item of items) {
+let dropdowns = document.getElementsByClassName("dropdown");
+for (let dropdown of dropdowns) {
+    let button = dropdown.children[0];
+    let items = dropdown.children[1].children;
+    for (let item of items) {
         item.addEventListener("click", function() {
             button.innerHTML = this.innerHTML;
         });
